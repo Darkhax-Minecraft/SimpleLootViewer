ee5683d161f754c2a447c20867cbdda6900aabcc
Tyler Hancock <darklime@live.ca>
2025-10-16 15:34:45 -0600

    Update build dependencies.

--- common/src/main/java/net/darkhax/simplelootviewer/common/impl/Content.java
+++ common/src/main/java/net/darkhax/simplelootviewer/common/impl/Content.java
@@ -1,25 +1,25 @@
 package net.darkhax.simplelootviewer.common.impl;
 
-import net.darkhax.bookshelf.common.api.registry.IContentProvider;
-import net.darkhax.bookshelf.common.api.registry.register.Register;
-import net.darkhax.bookshelf.common.api.registry.register.RegisterRecipeType;
+import net.darkhax.bookshelf.common.api.registry.ContentProvider;
+import net.darkhax.bookshelf.common.api.registry.adapters.GameRegistryAdapter;
+import net.darkhax.bookshelf.common.impl.registry.adapter.RecipeTypeAdapter;
 import net.darkhax.simplelootviewer.common.impl.data.LootDataProviderRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
-public class Content implements IContentProvider {
+public class Content implements ContentProvider {
 
     @Override
-    public void registerRecipeTypes(RegisterRecipeType registry) {
+    public void defineRecipeTypes(RecipeTypeAdapter registry) {
         registry.add("loot_table_info");
     }
 
     @Override
-    public void registerRecipeSerializers(Register<RecipeSerializer<?>> registry) {
+    public void defineRecipeSerializers(GameRegistryAdapter<RecipeSerializer<?>> registry) {
         registry.add("data", LootDataProviderRecipe.SERIALIZER);
     }
 
     @Override
-    public String contentNamespace() {
+    public String namespace() {
         return SimpleLootViewer.MOD_ID;
     }
 }
